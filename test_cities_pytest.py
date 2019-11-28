d249cb87db7bd6ed0e70796ec9941dbb53bad9bb
DJBD <57094924+DJBD@users.noreply.github.com>
2019-11-28 10:06:22 +0000

    computedistancetest

--- test_cities_pytest.py
+++ test_cities_pytest.py
@@ -3,30 +3,27 @@ from cities import *
 
 
 def test_compute_total_distance():
-    road_map1 = [("Kentucky", "Frankfort", 38.197274, -84.86311),\
-                ("Delaware", "Dover", 39.161921, -75.526755),\
-                ("Minnesota", "Saint Paul", 44.95, -93.094)]
-    
-    assert compute_total_distance(road_map1) == 11.111
+    #simple two city test (test to see if pythagoras formulae is working correctly (travels along the same path twice) (there and back again)
+    road_map1 = [("Kentucky", "Frankfort", 0, 0),("Delaware", "Dover", 12, 5)]
+    assert compute_total_distance(road_map1) == 26.0
 
-           
 
-    
+
 def test_swap_cities():
-    road_map2 = ([("STATE", "CITY", 121.121, 55.555),("STATE1", "CITY1", 39.161, -75.526)])
+    road_map2 = ([("STATE", "CITY",0.0, 0.0),("STATE1", "CITY1", 0.0, 0.0)])
 
-    index1 = 0
-    index2 = 1
+    assert swap_cities(road_map2,0,1) == ([("STATE1", "CITY1", 0.0, 0.0),("STATE", "CITY", 0.0, 0.0)],0.0)
 
-    assert swap_cities(road_map2) == ([("STATE", "CITY", 121.121, 55.555),\
-                ("STATE1", "CITY1", 39.161, -75.526)])
+
+
+
+
+
 
 def test_shift_cities():
-    road_map3 = ([("STATE", "CITY", 121.121, 55.555),\
-                ("STATE1", "CITY1", 39.161, -75.526)])
+    road_map3 = ([("STATE", "CITY",0.0, 0.0),("STATE1", "CITY1", 0.0, 0.0)])
 
-    assert shift_cities() == ([("STATE", "CITY", 121.121, 55.555),\
-                ("STATE1", "CITY1", 39.161, -75.526)])
+    assert shift_cities(road_map3) == [("STATE1", "CITY1", 0.0, 0.0),("STATE", "CITY", 0.0, 0.0)]
         
 
     
