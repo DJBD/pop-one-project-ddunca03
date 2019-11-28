066b70580861c5c3227af5a059e76b45e56c68a0
DJBD <57094924+DJBD@users.noreply.github.com>
2019-11-28 15:22:22 +0000

    Made a simple 4 city roadmap in order to easily test my functions

--- test_cities_pytest.py
+++ test_cities_pytest.py
@@ -1,29 +1,27 @@
 import pytest
 from cities import *
-
+simple_road_map = [("STATE", "CITY",0.0, 0.0),("STATE1", "CITY1", 12, 0.0),("STATE2", "CITY2", 12, 5.0),("STATE3", "CITY3", 0.0, 5.0)]
 
 def test_compute_total_distance():
-    #simple two city test (test to see if pythagoras formulae is working correctly (travels along the same path twice) (there and back again)
-    road_map1 = [("Kentucky", "Frankfort", 0, 0),("Delaware", "Dover", 12, 5)]
-    assert compute_total_distance(road_map1) == 26.0
+    #simple test to see if pythagoras formulae is working correctly
+    assert compute_total_distance(simple_road_map) == 34.0
 
 
+def test_swap_cities_1():
+    assert swap_cities(simple_road_map,0,1) == ([("STATE1", "CITY1", 12, 0.0),("STATE", "CITY",0.0, 0.0),("STATE2", "CITY2", 12, 5.0),("STATE3", "CITY3", 0.0, 5.0)], 50.0)
 
-def test_swap_cities():
-    road_map2 = ([("STATE", "CITY",0.0, 0.0),("STATE1", "CITY1", 0.0, 0.0)])
-
-    assert swap_cities(road_map2,0,1) == ([("STATE1", "CITY1", 0.0, 0.0),("STATE", "CITY", 0.0, 0.0)],0.0)
-
-
-
-
+    #write a test that swaps with itself/ should return the same roadmap
+    #write a test that deals with negitive indicies
+    #write a test that measures the length of the roadmap
 
 
 
 def test_shift_cities():
-    road_map3 = ([("STATE", "CITY",0.0, 0.0),("STATE1", "CITY1", 0.0, 0.0)])
+    assert shift_cities(simple_road_map) == [("STATE3", "CITY3", 0.0, 5.0),("STATE", "CITY",0.0, 0.0),("STATE1", "CITY1", 12, 0.0),("STATE2", "CITY2", 12, 5.0)]
 
-    assert shift_cities(road_map3) == [("STATE1", "CITY1", 0.0, 0.0),("STATE", "CITY", 0.0, 0.0)]
+    #write a test that swaps with itself/ should return the same roadmap
+    #write a test that deals with negitive indicies
+    #write a test that measures the length of the roadmap
         
 
     
